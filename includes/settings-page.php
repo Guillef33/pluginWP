5aa0c4dfcc5fed172dc87690cc73d53314d52abe
Guillermo <guillef33@gmail.com>
2025-01-12 11:34:41 -0300

    Add fields client id y client secret

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -14,17 +14,26 @@ function ml_add_settings_page() {
 function ml_render_settings_page() {
     ?>
 
-    <table class="form-table">
-        <tr valign="top">
-            <th scope="row">Client ID</th>
-            <td><input type="text" name="ml_client_id" value="<?php echo esc_attr(get_option('ml_client_id')); ?>" class="regular-text" /></td>
-        </tr>
-        <tr valign="top">
-            <th scope="row">Client Secret</th>
-            <td><input type="password" name="ml_client_secret" value="<?php echo esc_attr(get_option('ml_client_secret')); ?>" class="regular-text" /></td>
-        </tr>
-    </table>
-    <input type="submit" name="submit" id="submit" class="button button-primary" value="Guardar cambios">
+    <div class="wrap">
+        <h1>Ingresa tus credenciales</h1>
+        <form method="post" action="options.php">
+        <?php settings_fields('ml_settings_group'); ?>
+        <?php do_settings_sections('ml_settings_group'); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">Client ID</th>
+                <td><input type="text" name="ml_client_id" value="<?php echo esc_attr(get_option('ml_client_id')); ?>" class="regular-text" /></td>
+            </tr>
+            <tr valign="top">
+                <th scope="row">Client Secret</th>
+                <td><input type="password" name="ml_client_secret" value="<?php echo esc_attr(get_option('ml_client_secret')); ?>" class="regular-text" /></td>
+            </tr>
+        </table>
+        <input type="submit" name="submit" id="submit" class="button button-primary" value="Guardar cambios">
+    </form>
+    </div>
+
+   
 
     <div class="wrap">
         <h1>Conectar con Mercado Libre</h1>
