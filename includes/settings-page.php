158dce4ebedd60bf555b26c474881aeb494511b1
Guillermo <guillef33@gmail.com>
2025-01-12 11:30:18 -0300

    Fields for Client ID y Client Secret

--- includes/settings-page.php
+++ includes/settings-page.php
@@ -13,6 +13,19 @@ function ml_add_settings_page() {
 
 function ml_render_settings_page() {
     ?>
+
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row">Client ID</th>
+            <td><input type="text" name="ml_client_id" value="<?php echo esc_attr(get_option('ml_client_id')); ?>" class="regular-text" /></td>
+        </tr>
+        <tr valign="top">
+            <th scope="row">Client Secret</th>
+            <td><input type="password" name="ml_client_secret" value="<?php echo esc_attr(get_option('ml_client_secret')); ?>" class="regular-text" /></td>
+        </tr>
+    </table>
+    <input type="submit" name="submit" id="submit" class="button button-primary" value="Guardar cambios">
+
     <div class="wrap">
         <h1>Conectar con Mercado Libre</h1>
         <p>Haz clic en el siguiente botón para autenticarte con Mercado Libre:</p>
@@ -21,4 +34,6 @@ function ml_render_settings_page() {
         </a>
     </div>
     <?php
+
+    
 }
