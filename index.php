7acbb4173e6e007d2e35280913da35a3e88ca38d
Guillermo <guillef33@gmail.com>
2025-01-14 11:37:07 -0300

    activation and deactivation hooks

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 
-// Include WordPress core files.
-if ( ! function_exists( 'add_action' ) ) {
-	require_once( ABSPATH . 'wp-load.php' );
-}
+
 
 /**
  * Plugin Name:       Tus propiedades en Mercado Libre
@@ -16,6 +13,12 @@ if ( ! function_exists( 'add_action' ) ) {
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
+
+ // Include WordPress core files.
+if ( ! function_exists( 'add_action' ) ) {
+	require_once( ABSPATH . 'wp-load.php' );
+}
+
 // Define constantes.
 define( 'ML_AUTH_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ML_AUTH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
@@ -26,9 +29,61 @@ require_once ML_AUTH_PLUGIN_DIR . 'includes/functions.php';
 require_once ML_AUTH_PLUGIN_DIR . 'includes/settings-page.php';
 require_once ML_AUTH_PLUGIN_DIR . 'includes/micuenta-page.php';
 
-
 // Inicializa el plugin.
 add_action( 'plugins_loaded', [ 'Auth_Handler', 'init' ] );
+
+// Hooks de activación y desactivación
+register_activation_hook(__FILE__, 'ml_wp_register_activate');
+register_deactivation_hook(__FILE__, 'ml_wp_disable_plugin');
+
+function new_plugin_register_activate()
+{
+	update_option('_ml_wp_execute_activate', 1);
+}
+
+function new_plugin_disable_plugin()
+{
+	$GLOBALS['ml_wp']->disablePlugin();
+}
+
+// add_action( 'publish_post', 'send_notification', 10, 2 );
+
+// function send_notification( $id, $post_obj ) {
+//     // Armar el mensaje
+//     $msg = 'Se ha publicado un nuevo post: ';
+//     $msg .= '<strong>' . esc_html( $post_obj->post_title ) . '</strong><br>';
+//     $msg .= '<p>' . esc_html( wp_trim_words( $post_obj->post_content, 20, '...' ) ) . '</p>'; 
+//     $msg .= '<p>' . esc_html( wp_trim_words( $post_obj->post_author, 20, '...' ) ) . '</p>'; 
+//     $msg .= '<p>' . esc_html( wp_trim_words( $post_obj->post_date, 20, '...' ) ) . '</p>'; 
+//     $msg .= '<p>' . esc_html( wp_trim_words( $post_obj->post_status, 20, '...' ) ) . '</p>'; 
+
+
+//     // Guardar el mensaje en las opciones de WordPress
+//     update_option( 'mi_plugin_notificacion', $msg );
+// }
+
+// /**
+//  * Mostrar la notificación en la home
+//  */
+// function display_notification_on_home() {
+//     // Verificar si estamos en la home
+//     if ( is_front_page() ) {
+//         // Obtener el mensaje almacenado
+//         $msg = get_option( 'mi_plugin_notificacion' );
+
+//         // Mostrar el mensaje si existe
+//         if ( $msg ) {
+//             echo '<div style="background-color: #f9f9f9; border: 1px solid #ccc; padding: 10px; margin: 20px 0;">';
+//             echo '<h3>Notificación:</h3>';
+//             echo wp_kses_post( $msg );
+//             echo '</div>';
+//         }
+//     }
+// }
+// add_action( 'wp_footer', 'display_notification_on_home' );
+
+
+
 
 
 ?>
