e483ea528bd9bbef139212f3051fd2d87429dc91
Guillermo <guillef33@gmail.com>
2025-01-15 07:16:04 -0300

    test

--- templates/my_account_page.php
+++ templates/my_account_page.php
@@ -1,4 +1,14 @@
-<h1>Test Settings</h1>
+<div class="wrap">
+			<h1>Mi Cuenta en Mercado Libre</h1>
+			<p>Aquí podrás ver los detalles de tu cuenta de Mercado Libre:</p>
+
+			<?php
+			$access_token = Auth_Handler::handle_callback();
+
+				echo "El Access Token es: $access_token";
+
+			?>
+	</div>
 
 
 <?php
