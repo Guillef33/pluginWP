e8e304c69043313aeaa8a7f30bc780a67dc76090
Guillermo <guillef33@gmail.com>
2025-01-15 07:17:43 -0300

    views

--- templates/my_account_page.php
+++ templates/my_account_page.php
@@ -1,28 +1,12 @@
 <div class="wrap">
-			<h1>Mi Cuenta en Mercado Libre</h1>
-			<p>Aquí podrás ver los detalles de tu cuenta de Mercado Libre:</p>
+	<h1>Mi Cuenta en Mercado Libre</h1>
+	<p>Aquí podrás ver los detalles de tu cuenta de Mercado Libre:</p>
 
-			<?php
-			$access_token = Auth_Handler::handle_callback();
+	<?php
+	$access_token = Auth_Handler::handle_callback();
 
-				echo "El Access Token es: $access_token";
+		echo "El Access Token es: $access_token";
 
-			?>
-	</div>
+	?>
+</div>
 
-
-<?php
-function my_account_page() {?>
-	<div class="wrap">
-			<h1>Mi Cuenta en Mercado Libre</h1>
-			<p>Aquí podrás ver los detalles de tu cuenta de Mercado Libre:</p>
-
-			<?php
-			$access_token = Auth_Handler::handle_callback();
-
-				echo "El Access Token es: $access_token";
-
-			?>
-	</div>
-<?php
-}
